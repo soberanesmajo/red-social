ae9f38b9485a755bed1617bd55cbf646d2fc3b23
soberanesmajo <soberanesmajo@gmail.com>
2018-07-28 17:56:26 -0500

    agrega reciente actualización

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,70 +1,69 @@
-// Para autenticar con correo y contraseña
+// Para ingreso con correo y contraseña
 
-// Inicializar Firebase
+// Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyC9JZpmKLPBvfy3FN6n-HeHJ8g1ncj_xFM',
-  authDomain: 'red-social-98290.firebaseapp.com',
-  databaseURL: 'https://red-social-98290.firebaseio.com',
-  projectId: 'red-social-98290',
-  storageBucket: 'red-social-98290.appspot.com',
-  messagingSenderId: '186702904423'
+  apiKey: 'AIzaSyCwZtKNwCJJTU-oRr5j2LL921ZbKPP6ovQ',
+  authDomain: 'social-network-laboratoria.firebaseapp.com',
+  databaseURL: 'https://social-network-laboratoria.firebaseio.com',
+  projectId: 'social-network-laboratoria',
+  storageBucket: 'social-network-laboratoria.appspot.com',
+  messagingSenderId: '67671745906'
 };
 firebase.initializeApp(config);
 
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const login = document.getElementById('log-in');
-const signup = document.getElementById('sign-up');
-const logout = document.getElementById('log-out');
+const login = document.getElementById('login');
+const signupEmail = document.getElementById('signup-email');
+const signupPassword = document.getElementById('signup-password');
+const signup = document.getElementById('signup');
 
 // Agregar evento para el botón de inicio de sesión
 login.addEventListener('click', event => {
-// Obtenemos valor de email y password
+  event.preventDefault();
+  // console.log('hola');
+  // Obtenemos valor de email y password
   const emailValue = email.value;
   const passwordValue = password.value;
   const auth = firebase.auth();
   // Para iniciar sesión 
   const promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
   promise.catch(event => console.log(event.message));
+  location.href = 'views/muro.html';
 });
 
-// Añadiendo el evento del botón de registrarse
+// Para ingresar con google
 
-signup.addEventListener('click', event => {
-  const emailValue = email.value;
-  const passwordValue = password.value;
-  const auth = firebase.auth();
-
-  const promise = auth.createUserWithEmailAndPassword(emailValue, passwordValue);
-  promise.catch(event => console.log(event.message));
-});
-
-// Botón para cerrar sesión
-
-logout.addEventListener('click', event => {
-  firebase.auth().signOut();
-}); 
-
-firebase.auth().onAuthStateChanged(firebaseUser => {
-  if (firebaseUser) {
-    console.log(firebaseUser); // Este "console.log" es para verificar que se esté guardando la info de usuario.
-  } else {
-    console.log('not logged in');
-    logout.classList.add('hide');
-  } 
-});
-
-// Para autenticar con google
-
-const provider = new firebase.auth.GoogleAuthProvider();
 const loginGoogle = document.getElementById('login-google');
+const provider = new firebase.auth.GoogleAuthProvider();
 
 loginGoogle.addEventListener('click', event => {
+  event.preventDefault();
   firebase.auth()
-    .signInWithPopup(provider) // popUp te va a dar la ventana de acceso a tu cuenta de google. parámetro de la variable provider que tiene la autenticación con google
-    
+    .signInWithPopup(provider) // popUp te va a dar la ventana de acceso a tu cuenta de google. Parámetro de la variable provider que tiene la autenticación con google.
+
     .then(function(provider) { // entonces ejecuta la función que es el resultado (acceder con google)
+      location.href = 'views/muro.html';
       // console.log(result);
       // console.log(provider);
     });
 });
+
+// Para ingresar con facebook
+
+const loginFacebook = document.getElementById('login-facebook');
+const provFace = new firebase.auth.FacebookAuthProvider();
+// provFace.addScope('public_profile');
+
+loginFacebook.addEventListener('click', event => {
+  event.preventDefault();
+  firebase.auth()
+    .signInWithPopup(provFace) // popUp te va a dar la ventana de acceso a tu cuenta de facebook. Parámetro de la variable provFace que tiene la autenticación con facebook.
+
+    .then(function(provFace) { // entonces ejecuta la función que es el resultado (acceder con Facebook)
+      location.href = 'views/muro.html';
+      // console.log(result);
+      // console.log(provider);
+    });
+});
+
